d5a0c32d93f1a4acbb454db06f1b93aa40011926
DhanwisTechInfoSolutionKNR <156664258+dhanwis@users.noreply.github.com>
2024-07-10 09:13:00 +0530

    changes

--- customer_app/views.py
+++ customer_app/views.py
@@ -16,19 +16,6 @@ def about(request):
     }
     return render(request, 'customer_app/pages/about.html', context)
 
-# def services(request):
-#     current_page = 'services'
-#     services = Service.objects.all().order_by('-id')
-
-#     chunk_size = 4
-#     services_chunks = [services[i:i + chunk_size] for i in range(0, len(services), chunk_size)]
-
-#     context = {
-#         'current_page': current_page,
-#         'services': services,
-#         'services_chunks': services_chunks,
-#     }
-#     return render(request, 'customer_app/pages/services.html', context)
 
 def services(request):
     current_page = 'services'
